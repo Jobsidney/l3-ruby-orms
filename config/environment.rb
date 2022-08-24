7217bd168efea1d27cb9daaf2b5fd78bc11bddce
Jobsidney <jobsidney67@gmail.com>
2022-08-24 12:54:14 +0300

    UPdating the db

--- config/environment.rb
+++ config/environment.rb
@@ -9,5 +9,16 @@ DB = { conn: SQLite3::Database.new("db/school.db") }
 Student.create_table
 
 #create_student
-esther=Student.new(name: "Esther",age: 22)
+esther=Student.new(name: "Esther",age: 22,)
 esther.save
+job=Student.new(name: "Job",age: 22)
+job.save
+
+pp Student.show_all
+p "YESSS"
+esther.name="ANother Name"
+esther.age=22
+esther.update_record
+esther.delete_student
+
+pp Student.show_all
--- lib/student.rb
+++ lib/student.rb
@@ -27,21 +27,54 @@ def save
 end
 
 # TODO: SHOW ALL RECORDS
+def self.show_all
+    #class method so that it can see the instances of the class
+    query=<<-SQL
+        SELECT * FROM students
+    SQL
+    DB[:conn].execute(query).map{
+        |row|
+        wholeTable(row)
+    }
+end
 
-
-
+#show most recent
+def show_most_recent
+    query=<<-SQL
+        SELECT * FROM students ORDER BY name DESC LIMIT 1
+    SQL
+    DB[:conn].execute(query).map{
+        |row|
+        Student.wholeTable(row)
+    }
+end
 # TODO: UPDATE RECORD
+
+def update_record
+    query=<<-SQL
+        UPDATE students SET name = ?, age = ? WHERE id=?
+    SQL
+    DB[:conn].execute(query,self.name,self.age,self.id)
+end
 
 # TODO: DELETE RECORD
 
+def delete_student
+    query=<<-SQL
+    DELETE FROM students  WHERE id=?
+SQL
+DB[:conn].execute(query,self.id)
+end
+
 # TODO: CONVERT TABLE RECORD TO RUBY OBJECT
-def wholeTable(row)
+def self.wholeTable(row)
     Student.new(
         name:row[1],
         age:row[2],
         id: row[0]
     )
 end
+#the oldest
 
 # TODO: SEARCH FOR RECORD THAT MEETS CERTAIN CONDITIONS
 private
