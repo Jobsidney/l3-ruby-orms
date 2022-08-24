d13c051e4697121856df9119ead6e96496170f6d
Jobsidney <jobsidney67@gmail.com>
2022-08-24 12:26:01 +0300

    Save & retreve

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,8 @@ require_relative '../lib/student'
 DB = { conn: SQLite3::Database.new("db/school.db") }
 
 # RUN CODE FROM HERE
+Student.create_table
+
+#create_student
+esther=Student.new(name: "Esther",age: 22)
+esther.save
--- lib/student.rb
+++ lib/student.rb
@@ -6,19 +6,51 @@ class Student
         @name=name
         @id=id
     end
+    
 
 # TODO: CREATE TABLE
+def self.create_table
+    query= <<-SQL
+        CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARY KEY,name TEXT,age INTEGER
+        )
+    SQL
+    DB[:conn].execute(query)
+end
 
 # TODO: INSERT RECORD
+def save
+    query=<<-SQL
+        INSERT INTO students(name,age) VALUES(?,?)
+    SQL
+    DB[:conn].execute(query,self.name,self.age)
+    set_id
+end
 
 # TODO: SHOW ALL RECORDS
+
+
 
 # TODO: UPDATE RECORD
 
 # TODO: DELETE RECORD
 
 # TODO: CONVERT TABLE RECORD TO RUBY OBJECT
+def wholeTable(row)
+    Student.new(
+        name:row[1],
+        age:row[2],
+        id: row[0]
+    )
+end
 
 # TODO: SEARCH FOR RECORD THAT MEETS CERTAIN CONDITIONS
+private
+def set_id
+    query=<<-SQL
+        SELECT last_insert_rowid() FROM students
+    SQL
+    retreved_id=DB[:conn].execute(query)
+    self.id=retreved_id[0][0]
+end
 
 end
